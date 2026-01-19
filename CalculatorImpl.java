547f6a10dfd04bbd8fd3614eae9e9299ddd804cb
YeabkalTibebu <yeabkaltibebu@gmail.com>
2026-01-19 07:56:43 -0800

    Update CalculatorImpl.java

--- CalculatorImpl.java
+++ CalculatorImpl.java
@@ -1,37 +1,43 @@
-import java.rmi.*;
-import java.rmi.server.*;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 
-public class CalculatorImpl extends UnicastRemoteObject implements CalculatorInterface {
+public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
 
-    public CalculatorImpl() throws RemoteException {
+    protected CalculatorImpl() throws RemoteException {
         super();
     }
 
+    @Override
     public int add(int a, int b) throws RemoteException {
         return a + b;
     }
 
+    @Override
     public int subtract(int a, int b) throws RemoteException {
         return a - b;
     }
 
+    @Override
     public int multiply(int a, int b) throws RemoteException {
         return a * b;
     }
 
-    public int divide(int a, int b) throws RemoteException {
-        if (b == 0) throw new RemoteException("Division by zero");
+    @Override
+    public double divide(double a, double b) throws RemoteException {
+        if (b == 0) {
+            throw new RemoteException("Cannot divide by zero!");
+        }
         return a / b;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         try {
-            
-            CalculatorImpl server = new CalculatorImpl();
-            Naming.rebind("CalculatorService", server);
-            System.out.println("Server waiting.....");
+            Calculator calc = new CalculatorImpl();
+            Naming.rebind("CalculatorService", calc);
+            System.out.println("Calculator Server is running...");
         } catch (Exception e) {
-            System.out.println("Remote exception: " + e.toString());
+            System.err.println("Server error: " + e);
         }
     }
 }
